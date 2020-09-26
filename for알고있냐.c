dfba2411ab1cc4bf97056d023dca6ef00c676ca7
lego404 <71643183+lego404@users.noreply.github.com>
2020-09-26 21:48:59 +0900

    for문 알고있냐?
    백준 알고리즘 for문 합 구하기 문제를 풀어보았어요

---
+++ for알고있냐.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    int sum = 0;
+
+    scanf_s("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("%d", sum);
+
+    return 0;
+}
